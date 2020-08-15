7f83e96e4b2405de9dba3b2038971aae47f479fd
bonasunu <bonaventurasunu@gmail.com>
2020-08-15 15:03:59 +0700

    create advice generator

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios'
 
-function App() {
+const App = () => {
+  const [advice, setAdvice] = useState('')
+
+  const vibe = () => {
+    axios
+      .get('https://api.adviceslip.com/advice')
+      .then(response => {
+        setAdvice(response.data.slip.advice)
+      })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Maggr</h1>
+      <button onClick={vibe}>Check</button>
+      {advice}
     </div>
   );
 }
